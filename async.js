08f5b1aabed84ce655877b90be70b4af0bb5b215
Rashotech <rashotech@gmail.com>
2020-10-28 10:27:49 +0100

    Async Video Ass

--- async.js
+++ async.js
@@ -1,15 +1,15 @@
 
-loadVideo("aa")
+loadVideo("https://zaracarefoundation.com/wp-content/uploads/2020/10/y2mate.com-Master-KG-Jerusalema-Remix-Feat.-Burna-Boy-and-Nomcebo-Official-Music-Video_360p.mp4")
 videoContent = document.getElementById("video")
 async function loadVideo(src) {
     var x = document.createElement("VIDEO");
     try {
     x.setAttribute("src",src);
-    x.setAttribute("width", "320");
-    x.setAttribute("height", "240");
+    x.setAttribute("width", "640");
+    x.setAttribute("height", "500");
     x.setAttribute("controls", "controls");
+    await x.play()
     videoContent.appendChild(x);
-    await x.autoplay
     // vid.onloadstart = function() {
     // }; 
     } catch(error){
